870691ee80daa1e67da3549332f9ae2f37479462
Sado Gilles <sadowizzy@gmail.com>
2020-05-26 02:22:54 +0200

    Add project files.

---
+++ CalculatorWebApplication/WebService1.asmx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace CalculatorWebApplication
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public int Add(int a, int b)
+        {
+            return (a + b);
+        }
+
+        [WebMethod]
+        public System.Single Subtract(System.Single A, System.Single B)
+        {
+            return (A - B);
+        }
+
+        [WebMethod]
+        public System.Single Multiply(System.Single A, System.Single B)
+        {
+            return A * B;
+        }
+
+        [WebMethod]
+        public System.Single Divide(System.Single A, System.Single B)
+        {
+            if (B == 0)
+                return -1;
+            return Convert.ToSingle(A / B);
+        }
+    }
+}
